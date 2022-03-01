6fa99d45dc617c29c660d26bac720b0890978cdb
PeterT <tranpp87@gmail.com>
2022-03-01 16:01:38 -0500

    setup browserRouter and app navlink

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import {Route, Routes, NavLink} from 'react-router-dom'
+
+import './App.css';
+
+function App() {
+  const [pokemon, setPokemon] = useState([])
+
+  return (
+    <div className="App">
+      <header className='App-header'>
+        React Pokemon CRUD
+        <nav>
+          <NavLink to='/'>Pokemon List</NavLink>
+          <NavLink to='/add' className='m-left'> Add Pokemon</NavLink>
+        </nav>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
