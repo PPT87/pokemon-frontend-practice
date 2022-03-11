a83fef97d679c3e249b41fbae088d406fc517574
PeterT <tranpp87@gmail.com>
2022-03-10 21:53:19 -0500

    refactor create service to try/catch and async/await

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -3,15 +3,20 @@
 const BASE_URL = '/api/pokemon'
 
 //uploading JSON data
-const create = (pokemon) => {
-  return fetch(BASE_URL, {
-    method: "POST", 
-    headers: { //telling backend that "i'm sending you JSON data"
-      "content-type": "application/json"
-    },
-    body: JSON.stringify(pokemon)
-  })
-  .then(res => res.json())
+const create = async pokemon => {
+  try{
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: { //telling backend that "i'm sending you JSON data"
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(pokemon)
+    })
+    const data = await res.json()
+    return data
+  } catch (err) {
+    throw err
+  }
 }
 
 //get all pokemon function
@@ -32,10 +37,12 @@ const deleteOne = (id) => {
 const update = async (pokemon) => {
   const updatedPokemon = await fetch(`${BASE_URL}/${pokemon._id}`, {
     method: "PUT",
-    headers: { "content-type": "application/json" },
-    body: JSON.stringify(pokemon)
-  })
-  return await updatedPokemon.json() 
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(pokemon),
+  });
+  return await updatedPokemon.json()
 }
 
 export {
